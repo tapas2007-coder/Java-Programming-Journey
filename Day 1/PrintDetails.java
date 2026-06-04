529ff11d447041132928c9f83442819a9fa51bd7
Tapas Ranjan Behera <beheratapasranjan813@gmail.com>
2026-06-04 21:30:19 +0530

    Create PrintDetails.java

---
+++ Day 1/PrintDetails.java
@@ -0,0 +1,7 @@
+public class PrintDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: Tapas");
+        System.out.println("Age: 19");
+        System.out.println("City: Bhubaneswar");
+    }
+}
