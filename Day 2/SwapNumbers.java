80b8570178dbe3f0476d55df16e94dadb6320a7d
Tapas Ranjan Behera <beheratapasranjan813@gmail.com>
2026-06-05 16:19:22 +0530

    Create SwapNumbers.java

---
+++ Day 2/SwapNumbers.java
@@ -0,0 +1,14 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
