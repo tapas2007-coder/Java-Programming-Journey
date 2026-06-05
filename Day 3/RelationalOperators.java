86dd2b6308910bdeab86dc028202a5db38828fbb
Tapas Ranjan Behera <beheratapasranjan813@gmail.com>
2026-06-05 16:50:39 +0530

    Create RelationalOperators.java

---
+++ Day 3/RelationalOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
