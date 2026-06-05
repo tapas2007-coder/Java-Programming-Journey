13d72bd44f9bdcdbef5d78de282a6044477a6cbd
Tapas Ranjan Behera <beheratapasranjan813@gmail.com>
2026-06-05 17:08:30 +0530

    Create AreaOfCircle.java

---
+++ Day 4/AreaOfCircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();
+
+        double area = 3.14 * r * r;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
