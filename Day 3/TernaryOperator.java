9ced7547c3e3b823cadcb7f5a8dfad0b614704fb
Tapas Ranjan Behera <beheratapasranjan813@gmail.com>
2026-06-05 16:51:06 +0530

    Create TernaryOperator.java

---
+++ Day 3/TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 30;
+
+        int max = (a > b) ? a : b;
+
+        System.out.println("Maximum number is: " + max);
+    }
+}
