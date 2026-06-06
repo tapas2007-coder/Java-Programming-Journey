a28a86c48281fa2aa74ae944f0449d7bd5c32c43
Tapas Ranjan Behera <beheratapasranjan813@gmail.com>
2026-06-06 11:33:33 +0530

    Create Voting.java

---
+++ Day 5/Voting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Voting{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your Age: ");
+    int age = sc.nextInt();
+    if(age >= 18){
+      System.out.println("Eligible to vote");
+    }
+    sc.close();
+  }
+}
